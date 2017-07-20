1f985f6871ad0b25fa89ce291cc526eb9e240fe0
Yohann Streibel <yohann.streibel@myscript.com>
2017-07-20 08:45:16 +0200

    [DEV] Remove old client with ionic 2.0 beta version

--- client/src/models/schedule.ts
+++
@@ -1,9 +0,0 @@
-export class Schedule {
-  constructor(
-    public id?: number,
-    public day?: string,
-    public start?: string,
-    public end?: string,
-    public hallId?: number
-  ) {}
-}
